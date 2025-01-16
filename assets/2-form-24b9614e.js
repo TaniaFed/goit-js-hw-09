dd2e3cef377e444fc3d1ffc944d38bc1fac25ab6
TaniaFed <150553166+TaniaFed@users.noreply.github.com>
2025-01-16 19:17:07 +0000

    Deploying to gh-pages from @ TaniaFed/goit-js-hw-09@132b41168ef72352e8aa6e67312493e543831f6c 🚀

--- assets/2-form-24b9614e.js
+++ assets/2-form-24b9614e.js
@@ -1,2 +1,2 @@
 const t=document.querySelector(".feedback-form"),o={email:"",message:""},a="feedback-form-state";o.value=localStorage.getItem("localStorageKey")??"";t.addEventListener("input",e=>{localStorage.setItem(a,e.target.value)});t.addEventListener("submit",e=>{e.preventDefault(),console.log(o),localStorage.removeItem(a),t.reset()});
-//# sourceMappingURL=2-form-31809330.js.map
+//# sourceMappingURL=2-form-24b9614e.js.map
